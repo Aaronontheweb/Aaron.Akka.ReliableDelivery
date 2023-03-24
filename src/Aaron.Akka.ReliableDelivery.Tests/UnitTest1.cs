ab56978ae37b9bb56d6dff35d8dc7f7e261705c9
Aaron Stannard <aaron@petabridge.com>
2023-03-24 07:27:40 -0500

    initial commit

---
+++ src/Aaron.Akka.ReliableDelivery.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Aaron.Akka.ReliableDelivery.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/Aaron.Akka.ReliableDelivery/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Aaron.Akka.ReliableDelivery
+{
+    public class Class1
+    {
+    }
+}
