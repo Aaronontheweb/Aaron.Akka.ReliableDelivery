190b2baae5428cae3780b0d0d3d972d936f4a6f9
Aaron Stannard <aaron@petabridge.com>
2023-05-03 06:40:03 -0500

    fixed configuration issue

--- src/Aaron.Akka.ReliableDelivery.Cluster.Sharding/Internal/RdShardingConfig.cs
+++ src/Aaron.Akka.ReliableDelivery.Cluster.Sharding/Internal/RdShardingConfig.cs
@@ -4,11 +4,46 @@
 //      Copyright (C) 2013-2023 .NET Foundation <https://github.com/akkadotnet/akka.net>
 //  </copyright>
 // -----------------------------------------------------------------------
+
+using Aaron.Akka.ReliableDelivery.Internal;
 using Akka.Configuration;
 
 namespace Aaron.Akka.ReliableDelivery.Cluster.Sharding.Internal;
 
 public static class RdShardingConfig
 {
-    public static Config DefaultConfig() => ConfigurationFactory.FromResource<ShardingConsumerController.Settings>("Aaron.Akka.ReliableDelivery.Cluster.Sharding.delivery.conf");
+    public static Config DefaultConfig() => RdConfig.DefaultConfig().WithFallback(ConfigurationFactory.ParseString(@"
+        akka.reliable-delivery {
+  sharding {
+    producer-controller {
+      # Limit of how many messages that can be buffered when there
+      # is no demand from the consumer side.
+      buffer-size = 1000
+
+      # Ask timeout for sending message to worker until receiving Ack from worker
+      internal-ask-timeout = 60s
+
+      # If no messages are sent to an entity within this duration the
+      # ProducerController for that entity will be removed.
+      cleanup-unused-after = 120s
+
+      # In case ShardingConsumerController is stopped and there are pending
+      # unconfirmed messages the ShardingConsumerController has to ""wake up""
+# the consumer again by resending the first unconfirmed message.
+    resend-first-unconfirmed-idle-timeout = 10s
+
+# Chunked messages not implemented for sharding yet. Override to not
+# propagate property from akka.reliable-delivery.producer-controller.
+        chunk-large-messages = off
+}
+
+consumer-controller {
+# Limit of how many messages that can be buffered before the
+# ShardingConsumerController is initialized by the Start message.
+    buffer-size = 1000
+}
+}
+}
+
+    "));
 }
